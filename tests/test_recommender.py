ed07a1cfe69e5e402d22de7a2cfa03f846bb628b
hmasum52 <hasanmasum1852@gmail.com>
2026-04-15 01:07:18 -0400

    feat: add related_genres and related_moods to UserProfile in tests

--- tests/test_recommender.py
+++ tests/test_recommender.py
@@ -36,6 +36,8 @@ def test_recommend_returns_songs_sorted_by_score():
         favorite_mood="happy",
         target_energy=0.8,
         likes_acoustic=False,
+        related_genres=[],
+        related_moods=[],
     )
     rec = make_small_recommender()
     results = rec.recommend(user, k=2)
@@ -52,6 +54,8 @@ def test_explain_recommendation_returns_non_empty_string():
         favorite_mood="happy",
         target_energy=0.8,
         likes_acoustic=False,
+        related_genres=[],
+        related_moods=[],
     )
     rec = make_small_recommender()
     song = rec.songs[0]
